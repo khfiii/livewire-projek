7fe9bf59a2f57af47237b0aae811a21c5d7bfb9b
khfiii <khfii635@gmail.com>
2023-09-21 22:14:50 +0800

    change

--- resources/views/page/content.blade.php
+++ resources/views/page/content.blade.php
@@ -2,7 +2,7 @@
     <div class="w-full h-screen flex justify-center items-center">
         <div x-data="{tab : 'author'}">
            <nav>
-            <div class="tabs tabs-boxed">
+            <div class="tabs tabs-boxed p-2">
                 <a class="tab" @click="tab = 'author'"
                 :class = "{'tab-active' : tab == 'author'}"
 
@@ -14,29 +14,40 @@
                 <a class="tab" @click="tab= 'editor'"
                 :class = "{'tab-active' : tab == 'editor'}"
                 >Editor</a>
+
+                <a class="tab" @click="tab= 'user'"
+                :class = "{'tab-active' : tab == 'user'}"
+                >User</a>
             </div>
            </nav>
 
            <main>
-            <div class="h-[300px] w-[300px] debug mt-4 space-y-2 flex flex-col p-2"
+            <div class="h-[300px] w-[300px] mt-4 space-y-2 flex flex-col p-2"
             x-show = "tab == 'author'"
             >
                 <h2 class="font-semibold">Ini adalah author</h2>
                 <p>Ini adalah teks untuk author.</p>
             </div>
 
-            <div class="h-[300px] w-[300px] debug mt-4 space-y-2 flex flex-col p-2"
+            <div class="h-[300px] w-[300px] mt-4 space-y-2 flex flex-col p-2"
             x-show = "tab == 'reviewer'"
             >
                 <h2 class="font-semibold">Ini adalah reviewer</h2>
                 <p>Ini adalah teks untuk reviewer.</p>
             </div>
 
-            <div class="h-[300px] w-[300px] debug mt-4 space-y-2 flex flex-col p-2"
+            <div class="h-[300px] w-[300px] mt-4 space-y-2 flex flex-col p-2"
             x-show = "tab == 'editor'"
             >
                 <h2 class="font-semibold">Ini adalah editors</h2>
                 <p>Ini adalah teks untuk editor.</p>
+            </div>
+
+            <div class="h-[300px] w-[300px] mt-4 space-y-2 flex flex-col p-2"
+            x-show = "tab == 'user'"
+            >
+                {{-- <h2 class="font-semibold">Ini adalah editors</h2>
+                <p>Ini adalah teks untuk editor.</p> --}}
             </div>
            </main>
 
