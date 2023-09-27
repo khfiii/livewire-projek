4165c5eb58ff5ef7634c768ada402b598a640d25
khfiii <khfii635@gmail.com>
2023-09-27 21:27:26 +0800

    fix table

--- resources/views/livewire/form.blade.php
+++ resources/views/livewire/form.blade.php
@@ -1,9 +1,7 @@
-<div
-    class="flex flex-col lg:flex-row gap-10 h-screen justify-center items-center">
+<div class="flex flex-col lg:flex-row gap-10 h-screen justify-center items-center">
     <form wire:submit.prevent="submit">
         <div class="mb-6">
-            <label for="name"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                 Name</label>
             <input type="text" wire:model="name" id="name"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -14,13 +12,11 @@
 
         </div>
 
-        <div
-            class="font-semibold bg-green-400 container text-end leading-8 text-gray-50">
+        <div class="font-semibold bg-green-400 container text-end leading-8 text-gray-50">
         </div>
 
         <div class="mb-6">
-            <label for="email"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                 email</label>
             <input type="email" wire:model="email" id="email"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -30,8 +26,7 @@
             @enderror
         </div>
         <div class="mb-6">
-            <label for="password"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                 password</label>
             <input type="password" wire:model="password" id="password"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
@@ -44,8 +39,7 @@
                 <input id="remember" type="checkbox" value=""
                     class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800">
             </div>
-            <label for="remember"
-                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember
+            <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember
                 me</label>
         </div>
         <button type="submit"
@@ -54,49 +48,34 @@
 
     <div class="overflow-x-auto mt-2 flex flex-col gap-2">
         <div class="inline-flex gap-2">
-            <input type="text" placeholder="Type here"
-                class="input input-bordered input-primary w-full max-w-md"
+            <input type="text" placeholder="Type here" class="input input-bordered input-primary w-full max-w-md"
                 wire:model.live="search" />
         </div>
 
-        <div class="h-[400px] w-[500px]">
-          @if (count($users) > 0)
+        <div class="h-full debug w-full">
 
-          <table class="table">
+           @if (count($users)>0)
+           <table class="table">
             <!-- head -->
             <thead>
                 <tr>
                     <th></th>
                     <th>Name</th>
                     <th>Email</th>
+                    <th>Action</th>
+
                 </tr>
             </thead>
-            <tbody>
+            <tbody class="debug">
                 @foreach ($users as $user)
-                    <tr wire:key="{{ $user->id }}">
-                        <th>{{ $loop->iteration }}</th>
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
                         <td>{{ $user->name }}</td>
                         <td> {{ $user->email }} </td>
-                        <td><button class="btn btn-sm btn-primary">Update</button></td>
-                          <table class="table">
-                <!-- head -->
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Name</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($users as $user)
-                        <tr wire:key="{{ $user->id }}">
-                            <th>{{ $loop->iteration }}</th>
-                            <td>{{ $user->name }}</td>
-                            <td> {{ $user->email }} </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                        <td>
+                            <button class="btn btn-sm btn-primary">Update</button>
+                            <button class="btn btn-sm btn-warning">Delete</button>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
@@ -104,10 +83,9 @@
 
         @else
 
-        <div class="badge badge-primary mx-auto">Users not found...</div>
+        <div class="mt-4 w-[300px] h-[200px] relative overflow-auto">Nothing Users...</div>
 
-          @endif
+           @endif
         </div>
     </div>
 </div>
-
